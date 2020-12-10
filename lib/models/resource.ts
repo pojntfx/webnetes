df8fa7cd8300570f87c87aaeedcc06724e82d458
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-12-10 22:12:12 +0000

    feat: Start adding resources

---
+++ lib/models/resource.ts
@@ -0,0 +1,17 @@
+export const API_VERSION = "webnetes.felicitas.pojtinger.com/v1alpha1";
+
+export enum EResourceKind {
+  RUNTIME = "Runtime",
+}
+
+export interface IResourceMetadata {
+  name: string;
+  label: string;
+}
+
+export interface IResource<T> {
+  apiVersion: string;
+  kind: EResourceKind;
+  metadata: IResourceMetadata;
+  spec: T;
+}
