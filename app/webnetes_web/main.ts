a16f890bbd45ee04a31dee3449043973d17c484c
Felix Pojtinger <felix@pojtinger.com>
2020-12-13 22:12:34 +0000

    feat: Add example client & server resources

--- app/webnetes_web/main.ts
+++ app/webnetes_web/main.ts
@@ -4,29 +4,11 @@ import { IResource } from "../../lib/models/resource";
 
 (window as any).setImmediate = window.setInterval; // Polyfill
 
-const demoResources = `apiVersion: webnetes.felix.pojtinger.com/v1alpha1
-kind: Runtime
-metadata:
-  name: Generic WASI
-  label: wasi_generic
----
-apiVersion: webnetes.felix.pojtinger.com/v1alpha1
-kind: Runtime
-metadata:
-  name: TinyGo WASI
-  label: wasi_tinygo
----
-apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+const exampleServerResources = `apiVersion: webnetes.felix.pojtinger.com/v1alpha1
 kind: Runtime
 metadata:
   name: Go JSSI
   label: jssi_go
----
-apiVersion: webnetes.felix.pojtinger.com/v1alpha1
-kind: Runtime
-metadata:
-  name: TinyGo JSSI
-  label: jssi_tinygo
 ---
 apiVersion: webnetes.felix.pojtinger.com/v1alpha1
 kind: Capability
@@ -37,12 +19,151 @@ spec:
   privileged: true
 ---
 apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: Processor
+metadata:
+  name: Felix's iPhone
+  label: felixs_iphone
+spec:
+  runtimes:
+  - jssi_go
+  capabilities:
+  - bind_alias
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: StunServer
+metadata:
+  name: Google STUN Server
+  label: google
+spec:
+  urls:
+  - stun:stun.l.google.com:19302
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: StunServer
+metadata:
+  name: Twillio STUN Server
+  label: twillio
+spec:
+  urls:
+  - stun:global.stun.twilio.com:3478?transport=udp
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: TurnServer
+metadata:
+  name: Twillio TURN Server
+  label: twillio
+spec:
+  urls:
+  - turn:global.turn.twilio.com:3478?transport=tcp
+  username: f4b4035eaa76f4a55de5f4351567653ee4ff6fa97b50b6b334fcc1be9c27212d
+  credential: w1uxM55V9yVoqyVFjt+mxDBV0F87AUCemaYVQGxsPLw=
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: Signaler
+metadata:
+  name: Public unisockets Signaling Server
+  label: unisockets_public
+spec:
+  urls:
+  - wss://unisockets.herokuapp.com
+  retryAfter: 1000
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: Network
+metadata:
+  name: Public unisockets network
+  label: unisockets_public
+spec:
+  signaler: unisockets_public
+  stunServers:
+  - google
+  - twillio
+  turnServers:
+  - twillio
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: Subnet
+metadata:
+  name: Echo Network
+  label: echo_network
+spec:
+  network: unisockets_public
+  prefix: 127.0.0
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: Tracker
+metadata:
+  name: OpenWebTorrent
+  label: openwebtorrent
+spec:
+  urls:
+  - wss://tracker.openwebtorrent.com
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: Tracker
+metadata:
+  name: Fastcast
+  label: fastcast
+spec:
+  urls:
+  - wss://tracker.fastcast.nz
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: Repository
+metadata:
+  name: Public WebTorrent
+  label: webtorrent_public
+spec:
+  trackers:
+  - openwebtorrent
+  - fastcast
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: File
+metadata:
+  name: Go Echo Server Binary
+  label: go_echo_server
+spec:
+  repository: webtorrent_public
+  uri: magnet:?xt=urn:btih:f5a6d3714d888711b32b32a5afff7f2db27113d7&dn=echo_server.wasm&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: Arguments
+metadata:
+  name: Echo Server Configuration
+  label: echo_server
+spec:
+  argv:
+  - "-laddr"
+  - 127.0.0.1:1234
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: Workload
+metadata:
+  name: Go Echo Server
+  label: go_echo_server
+spec:
+  file: go_echo_server
+  runtime: jssi_go
+  capabilities:
+  - bind_alias
+  subnet: echo_network
+  arguments: echo_server
+`;
+
+const exampleClientResources = `apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+kind: Runtime
+metadata:
+  name: Go JSSI
+  label: jssi_go
+---
+apiVersion: webnetes.felix.pojtinger.com/v1alpha1
 kind: Capability
 metadata:
   name: Connecting to aliases
   label: connect_to_alias
 spec:
-  privileged: false
+  privileged: true
 ---
 apiVersion: webnetes.felix.pojtinger.com/v1alpha1
 kind: Processor
@@ -51,12 +172,8 @@ metadata:
   label: felixs_iphone
 spec:
   runtimes:
-  - wasi_generic
-  - wasi_tinygo
   - jssi_go
-  - jssi_tinygo
   capabilities:
-  - bind_alias
   - connect_to_alias
 ---
 apiVersion: webnetes.felix.pojtinger.com/v1alpha1
@@ -151,64 +268,37 @@ spec:
 apiVersion: webnetes.felix.pojtinger.com/v1alpha1
 kind: File
 metadata:
-  name: C Echo Server Binary
-  label: c_echo_server
+  name: Go Echo Client Binary
+  label: go_echo_client
 spec:
   repository: webtorrent_public
-  uri: magnet:?xt=urn:btih:9c2a3309dedf5a934569a40b0a739fb85e05f3ef&dn=echo_server_c.wasm&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com
----
-apiVersion: webnetes.felix.pojtinger.com/v1alpha1
-kind: File
-metadata:
-  name: TinyGo WASI Echo Server Binary
-  label: tinygo_wasi_echo_server
-spec:
-  repository: webtorrent_public
-  uri: magnet:?xt=urn:btih:a035f32c84233c3b8f465d7b4ba3a08b31fb8a55&dn=echo_server_wasi.wasm&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com
----
-apiVersion: webnetes.felix.pojtinger.com/v1alpha1
-kind: File
-metadata:
-  name: TinyGo JSSI Echo Server Binary
-  label: tinygo_jssi_echo_server
-spec:
-  repository: webtorrent_public
-  uri: magnet:?xt=urn:btih:fbe657b4f98ecc59c7af09af955f2bcde337cd71&dn=echo_server_jssi.wasm&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337
----
-apiVersion: webnetes.felix.pojtinger.com/v1alpha1
-kind: File
-metadata:
-  name: Go Echo Server Binary
-  label: go_echo_server
-spec:
-  repository: webtorrent_public
-  uri: magnet:?xt=urn:btih:f5a6d3714d888711b32b32a5afff7f2db27113d7&dn=echo_server.wasm&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337
+  uri: magnet:?xt=urn:btih:251cc6a9a733ea8b44b5cad0827895bb194f0e27&dn=echo_client.wasm&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com
 ---
 apiVersion: webnetes.felix.pojtinger.com/v1alpha1
 kind: Arguments
 metadata:
   name: Echo Server Configuration
-  label: echo_server
+  label: echo_client
 spec:
   argv:
-  - "-laddr"
+  - "-raddr"
   - 127.0.0.1:1234
 ---
 apiVersion: webnetes.felix.pojtinger.com/v1alpha1
 kind: Workload
 metadata:
-  name: Go Echo Server
-  label: go_echo_server
+  name: Go Echo Client
+  label: go_echo_client
 spec:
-  file: go_echo_server
+  file: go_echo_client
   runtime: jssi_go
   capabilities:
-  - bind_alias
+  - connect_to_alias
   subnet: echo_network
-  arguments: echo_server
+  arguments: echo_client
 `;
 
-const demoManagerConfig = `apiVersion: webnetes.felix.pojtinger.com/v1alpha1
+const exampleManagerConfig = `apiVersion: webnetes.felix.pojtinger.com/v1alpha1
 kind: StunServer
 metadata:
   name: Google STUN Server
@@ -256,9 +346,13 @@ spec:
   network: ""
   prefix: 127.0.0`;
 
-(document.getElementById(
-  "manager-config-input"
-) as HTMLTextAreaElement).value = demoManagerConfig;
+document
+  .getElementById("load-example-manager-config")
+  ?.addEventListener("click", async () => {
+    (document.getElementById(
+      "manager-config-input"
+    ) as HTMLTextAreaElement).value = exampleManagerConfig;
+  });
 
 document.getElementById("start")?.addEventListener("click", async () => {
   (document.getElementById("management") as HTMLTextAreaElement).style.cssText =
@@ -326,7 +420,19 @@ document.getElementById("start")?.addEventListener("click", async () => {
     );
   });
 
-  (document.getElementById(
-    "resource-input"
-  ) as HTMLTextAreaElement).value = demoResources;
+  document
+    .getElementById("load-example-server-resources")
+    ?.addEventListener("click", async () => {
+      (document.getElementById(
+        "resource-input"
+      ) as HTMLTextAreaElement).value = exampleServerResources;
+    });
+
+  document
+    .getElementById("load-example-client-resources")
+    ?.addEventListener("click", async () => {
+      (document.getElementById(
+        "resource-input"
+      ) as HTMLTextAreaElement).value = exampleClientResources;
+    });
 });
