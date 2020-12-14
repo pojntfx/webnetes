24dd6da4bded5de53d4bfb7b077cdc78f5932180
Felix Pojtinger <felix@pojtinger.com>
2020-12-14 22:20:04 +0000

    feat: List resources after creation, integrate seeding into worker resources

--- lib/aggregates/worker.ts
+++ lib/aggregates/worker.ts
@@ -493,19 +493,26 @@ export class Worker {
           case EResourceKind.FILE: {
             const file = resource as File;
 
-            const repo = this.getInstance<FileRepository>(
-              file.spec.repository,
-              API_VERSION,
-              EResourceKind.REPOSITORY
-            );
+            try {
+              const repo = this.getInstance<FileRepository>(
+                file.spec.repository,
+                API_VERSION,
+                EResourceKind.REPOSITORY
+              );
 
-            await repo.remove(file.spec.uri);
+              await repo.remove(file.spec.uri);
 
-            this.deleteInstance(
-              resource.metadata.label,
-              API_VERSION,
-              EResourceKind.FILE
-            );
+              this.deleteInstance(
+                resource.metadata.label,
+                API_VERSION,
+                EResourceKind.FILE
+              );
+            } catch (e) {
+              // Not an error if it hasn't been added/seeded yet
+              if (!(e instanceof InstanceDoesNotExistError)) {
+                throw e;
+              }
+            }
 
             break;
           }
@@ -533,7 +540,23 @@ export class Worker {
       EResourceKind.REPOSITORY
     );
 
-    return await repo.seed(content);
+    const magnetURI = await repo.seed(content);
+
+    const resource = new File(
+      { label: magnetURI, name: magnetURI },
+      { repository: repositoryLabel, uri: magnetURI }
+    );
+
+    this.setInstance<Uint8Array>(
+      resource.metadata.label,
+      API_VERSION,
+      EResourceKind.FILE,
+      content
+    );
+
+    this.resources.push(resource);
+
+    return resource;
   }
 
   private compareResources(actual: IResource<any>, expected: IResource<any>) {
