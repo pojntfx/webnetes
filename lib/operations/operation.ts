20289e510dc7b9587e24627997a99317967ebd13
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-12-15 22:42:08 +0000

    feat: Introduce IO modification operation

--- lib/operations/operation.ts
+++ lib/operations/operation.ts
@@ -1,12 +1,17 @@
+import { IIOModificationData } from "./io-modification";
 import { IModificationData } from "./modification";
 import { IModificationConfirmationData } from "./modification-confirmation";
 
 export enum EMANAGEMENT_OPCODES {
   MODIFICATION = "modification",
   MODIFICATION_CONFIRMATION = "modificationConfirmation",
+  IO_MODIFICATION = "ioModification",
 }
 
-export type TManagementData = IModificationData | IModificationConfirmationData;
+export type TManagementData =
+  | IModificationData
+  | IModificationConfirmationData
+  | IIOModificationData;
 
 export interface IManagementOperation<T> {
   opcode: EMANAGEMENT_OPCODES;
