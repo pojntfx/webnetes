c75f3874184116dd86438ed35e18bbad1a91ded5
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-12-16 20:35:39 +0000

    feat: Start implementation of transporter handling

--- lib/pipes/peer-pipe.ts
+++ lib/pipes/peer-pipe.ts
@@ -22,16 +22,42 @@ export enum EPeerPipeResourceTypes {
 export class PeerPipe
   implements IPipe<IPeerPipeConfig, EPeerPipeResourceTypes> {
   private logger = getLogger();
-
   private bus = new Emittery();
-
-  private ioFrameQueue = [] as IIOFrame[];
+  private ioFrameQueue = [] as Uint8Array[];
   private ioFrameTranscoder = new IOFrameTranscoder();
-
   private transporter?: Transporter;
+  private aliases = new Map<string, string>();
+  private nodes = [] as string[];
 
   async open(config: IPeerPipeConfig) {
     this.logger.debug("Opening PeerPipe", { config });
+
+    const transporter = new Transporter(
+      config.networkConfig,
+      async () => {},
+      async () => {},
+      async (nodeId: string) => {
+        this.logger.debug("Connected to peer", { nodeId });
+
+        this.nodes.push(nodeId);
+
+        while (this.nodes.includes(nodeId)) {
+          this.logger.silly("Received from peer", { nodeId });
+
+          const msg = await transporter.recv(nodeId);
+
+          this.ioFrameQueue.push(msg);
+          this.bus.emit(this.getReadKey(), msg);
+        }
+      },
+      async (nodeId) => {
+        this.logger.debug("Disconnected from peer", { nodeId });
+
+        this.nodes = this.nodes.filter((candidate) => candidate !== nodeId);
+      }
+    );
+
+    this.transporter = transporter;
   }
 
   async close() {
@@ -43,14 +69,16 @@ export class PeerPipe
   async read() {
     this.logger.debug("Reading from PeerPipe");
 
-    let frame: IIOFrame;
+    let msg: Uint8Array;
     if (this.ioFrameQueue.length !== 0) {
-      frame = this.ioFrameQueue.shift()!;
+      msg = this.ioFrameQueue.shift()!;
     } else {
-      frame = (await this.bus.once(this.getReadKey())) as IIOFrame;
+      msg = (await this.bus.once(this.getReadKey())) as Uint8Array;
 
       this.ioFrameQueue.shift();
     }
+
+    const frame = this.ioFrameTranscoder.decode(msg);
 
     return frame;
   }
@@ -97,6 +125,8 @@ export class PeerPipe
       });
 
       await this.transporter.send(nodeId, frame);
+
+      this.logger.silly("Sent to peer", { nodeId });
     } else {
       throw new ClosedError("transporter");
     }
