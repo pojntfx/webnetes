efe92c894c99bc9e841d2e4da089123a00502d48
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-12-16 01:04:51 +0000

    feat: Define states in DRP protocol

--- distributed-resource-pipe.rfc.ts
+++ distributed-resource-pipe.rfc.ts
@@ -1,15 +1,17 @@
-interface IDistributedResourcePipe<C, T> {
+interface IDistributedResourcePipe<C, T, H> {
   open: (config: C) => Promise<void>;
   close: () => Promise<void>;
   read: () => Promise<{
     resourceType: T;
     resourceId: string;
+    statusType: H;
     msg: Uint8Array;
     nodeId: string;
   }>;
   write: (
     resourceType: T,
     resourceId: string,
+    statusType: H,
     msg: Uint8Array,
     nodeId: string
   ) => Promise<void>;
@@ -22,45 +24,62 @@ class UnknownResourceError extends Error {
 }
 
 enum EResourcePipeResourceTypes {
-  PROCESS = "webnetes.felicitas.pojtinger.com/v1alpha1/process",
-  TERMINAL = "webnetes.felicitas.pojtinger.com/v1alpha1/terminal",
+  PROCESS = "webnetes.felicitas.pojtinger.com/v1alpha1/resources/process",
+  TERMINAL = "webnetes.felicitas.pojtinger.com/v1alpha1/resources/terminal",
 }
 
 enum EPeerPipeResourceTypes {
-  STDOUT = "webnetes.felicitas.pojtinger.com/v1alpha1/stdout",
-  STDIN = "webnetes.felicitas.pojtinger.com/v1alpha1/stdin",
+  STDOUT = "webnetes.felicitas.pojtinger.com/v1alpha1/resources/stdout",
+  STDIN = "webnetes.felicitas.pojtinger.com/v1alpha1/resources/stdin",
+}
+
+enum ECommonStateTypes {
+  PENDING = "webnetes.felicitas.pojtinger.com/v1alpha1/states/pending",
+  RESOLVED = "webnetes.felicitas.pojtinger.com/v1alpha1/states/resolved",
+  REJECTED = "webnetes.felicitas.pojtinger.com/v1alpha1/states/rejected",
 }
 
 class ResourcePipe
-  implements IDistributedResourcePipe<{}, EResourcePipeResourceTypes> {
+  implements
+    IDistributedResourcePipe<
+      {},
+      EResourcePipeResourceTypes,
+      ECommonStateTypes
+    > {
   open!: (config: {}) => Promise<void>;
   close!: () => Promise<void>;
   read!: () => Promise<{
     resourceType: EResourcePipeResourceTypes;
     resourceId: string;
+    statusType: ECommonStateTypes;
     msg: Uint8Array;
     nodeId: string;
   }>;
   write!: (
     resourceType: EResourcePipeResourceTypes,
     resourceId: string,
+    statusType: ECommonStateTypes,
     msg: Uint8Array,
     nodeId: string
   ) => Promise<void>;
 }
 
-class PeerPipe implements IDistributedResourcePipe<{}, EPeerPipeResourceTypes> {
+class PeerPipe
+  implements
+    IDistributedResourcePipe<{}, EPeerPipeResourceTypes, ECommonStateTypes> {
   open!: (config: {}) => Promise<void>;
   close!: () => Promise<void>;
   read!: () => Promise<{
     resourceType: EPeerPipeResourceTypes;
     resourceId: string;
+    statusType: ECommonStateTypes;
     msg: Uint8Array;
     nodeId: string;
   }>;
   write!: (
     resourceType: EPeerPipeResourceTypes,
     resourceId: string,
+    statusType: ECommonStateTypes,
     msg: Uint8Array,
     nodeId: string
   ) => Promise<void>;
@@ -78,6 +97,7 @@ async () => {
         const {
           resourceType,
           resourceId,
+          statusType,
           msg,
           nodeId,
         } = await resources.read();
@@ -87,6 +107,7 @@ async () => {
             await peers.write(
               EPeerPipeResourceTypes.STDOUT,
               resourceId,
+              ECommonStateTypes.PENDING,
               msg,
               nodeId // ID of node with process resource
             );
@@ -98,6 +119,7 @@ async () => {
             await peers.write(
               EPeerPipeResourceTypes.STDIN,
               resourceId,
+              ECommonStateTypes.PENDING,
               msg,
               nodeId // ID of node with terminal resource
             );
@@ -120,27 +142,71 @@ async () => {
   (async () => {
     try {
       while (true) {
-        const { resourceType, resourceId, msg, nodeId } = await peers.read();
+        const {
+          resourceType,
+          resourceId,
+          statusType,
+          msg,
+          nodeId,
+        } = await peers.read();
 
         switch (resourceType) {
           case EPeerPipeResourceTypes.STDOUT: {
-            await resources.write(
-              EResourcePipeResourceTypes.TERMINAL,
-              resourceId,
-              msg,
-              nodeId // ID of node with stdout resource
-            );
+            try {
+              await resources.write(
+                EResourcePipeResourceTypes.TERMINAL,
+                resourceId,
+                statusType,
+                msg,
+                nodeId // ID of node with stdout resource
+              );
+
+              await peers.write(
+                resourceType,
+                resourceId,
+                ECommonStateTypes.RESOLVED,
+                msg,
+                nodeId // ID of node with stdout resource
+              );
+            } catch (e) {
+              await peers.write(
+                resourceType,
+                resourceId,
+                ECommonStateTypes.REJECTED,
+                msg,
+                nodeId // ID of node with stdout resource
+              );
+            }
 
             break;
           }
 
           case EPeerPipeResourceTypes.STDIN: {
-            await resources.write(
-              EResourcePipeResourceTypes.PROCESS,
-              resourceId,
-              msg,
-              nodeId // ID of node with stdin resource
-            );
+            try {
+              await resources.write(
+                EResourcePipeResourceTypes.PROCESS,
+                resourceId,
+                statusType,
+                msg,
+                nodeId // ID of node with stdin resource
+              );
+
+              await peers.write(
+                resourceType,
+                resourceId,
+                ECommonStateTypes.RESOLVED,
+                msg,
+                nodeId // ID of node with stdout resource
+              );
+            } catch (e) {
+              await peers.write(
+                resourceType,
+                resourceId,
+                ECommonStateTypes.REJECTED,
+                msg,
+                nodeId // ID of node with stdout resource
+              );
+            }
 
             break;
           }
