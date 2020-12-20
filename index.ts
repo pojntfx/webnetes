224f00fad2c38c20f013bc1041ba74a26924cbd6
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-12-20 19:38:08 +0000

    feat: Export all resources necessary to manually implement a node

--- index.ts
+++ index.ts
@@ -1,9 +1,31 @@
-export { Manager } from "./lib/.deprecated/aggregates/manager";
-export { Worker } from "./lib/.deprecated/aggregates/worker";
-export { NetworkInterface } from "./lib/controllers/network-interface";
-export { FileRepository } from "./lib/controllers/file-repository";
+export { APIVersionNotImplementedError } from "./lib/errors/apiversion-not-implemented";
+export { ClosedError } from "./lib/errors/closed";
+export { ConfigMissingError } from "./lib/errors/config-missing";
+export { ResourceNotImplementedError } from "./lib/errors/resource-not-implemented";
+export { EPeersResources, Peers } from "./lib/pipes/peers";
+export { EResourcesResources, Resources } from "./lib/pipes/resources";
+export { Files } from "./lib/repositories/files";
+export { Processors } from "./lib/repositories/processors";
+export { Subnets } from "./lib/repositories/subnets";
+export { Workloads } from "./lib/repositories/workloads";
+export { Arguments } from "./lib/resources/arguments";
+export { Capability } from "./lib/resources/capability";
+export { File } from "./lib/resources/file";
+export { Network } from "./lib/resources/network";
+export { Processor } from "./lib/resources/processor";
+export { Repository } from "./lib/resources/repository";
 export {
-  VirtualMachine,
-  ECapabilities,
-  ERuntimes,
-} from "./lib/controllers/virtual-machine";
+  API_VERSION,
+  EResourceKind,
+  IResource,
+} from "./lib/resources/resource";
+export { Runtime } from "./lib/resources/runtime";
+export { Signaler } from "./lib/resources/signaler";
+export { StunServer } from "./lib/resources/stunserver";
+export { Subnet } from "./lib/resources/subnet";
+export { Tracker } from "./lib/resources/tracker";
+export { TurnServer } from "./lib/resources/turnserver";
+export { Workload } from "./lib/resources/workload";
+export { Frame } from "./lib/utils/frame-transcoder";
+export { ResourceTranscoder } from "./lib/utils/resource-transcoder";
+export { Node } from "./lib/high-level/node";
