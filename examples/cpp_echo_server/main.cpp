4be6551ccd3716bc7fd159e5557abb46214d7937
Felix Pojtinger <felix@pojtinger.com>
2020-12-22 20:55:12 +0000

    feat: Parse address and create socket in C++ example

--- examples/cpp_echo_server/main.cpp
+++ examples/cpp_echo_server/main.cpp
@@ -1,6 +1,12 @@
+#include <arpa/inet.h>
 #include <cstdint>
 #include <iostream>
+#include <netinet/in.h>
+#include <stdexcept>
+#include <stdlib.h>
+#include <string.h>
 #include <string>
+#include <sys/socket.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
@@ -24,10 +30,30 @@ int main(int argc, char *argv[]) {
     }
 
     default: {
-      std::cout << "Usage: " << argv[0] << " [-lp]\n";
+      std::cout << "Usage: " << argv[0] << " [-lp]" << std::endl;
 
-      std::exit(EXIT_FAILURE);
+      return EXIT_FAILURE;
     }
     }
+  }
+
+  // Address
+  sockaddr_in server_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(listen_port);
+  if (inet_pton(AF_INET, listen_host.c_str(), &server_address.sin_addr) == -1) {
+    std::cout << "[ERROR] Could not parse IP address: " << strerror(errno)
+              << std::endl;
+
+    return EXIT_FAILURE;
+  }
+
+  // Socket
+  int server_socket;
+  if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    std::cout << "[ERROR] Could not create socket: " << strerror(errno)
+              << std::endl;
+
+    return EXIT_FAILURE;
   }
 }
