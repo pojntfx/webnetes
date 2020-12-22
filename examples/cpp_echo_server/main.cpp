06bb2cbbb6a87cf1cc87a8ec15209d5efb080294
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-12-22 20:10:02 +0000

    feat: Parse flags in C++ example

--- examples/cpp_echo_server/main.cpp
+++ examples/cpp_echo_server/main.cpp
@@ -1,3 +1,33 @@
+#include <cstdint>
 #include <iostream>
+#include <string>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) { std::cout << "Hello, world!\n"; }
+int main(int argc, char *argv[]) {
+  // Flags
+  std::string listen_host = "127.0.0.1";
+  int listen_port = 1234;
+
+  int opt;
+  while ((opt = getopt(argc, argv, "l:p:") != -1)) {
+    switch (opt) {
+    case 'l': {
+      listen_host = optarg;
+
+      break;
+    }
+
+    case 'p': {
+      listen_port = std::stoi(optarg);
+
+      break;
+    }
+
+    default: {
+      std::cout << "Usage: " << argv[0] << " [-lp]\n";
+
+      std::exit(EXIT_FAILURE);
+    }
+    }
+  }
+}
