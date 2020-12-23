af6199f834f051395175973654b3112388d1ac9a
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-12-23 17:58:28 +0000

    refactor: Port to unisockets 0.0.23

--- examples/go_echo_server/go.mod
+++ examples/go_echo_server/go.mod
@@ -2,4 +2,4 @@ module github.com/pojntfx/webnetes/examples/go_echo_server
 
 go 1.15
 
-require github.com/pojntfx/tinynet v0.0.0-20201221193624-5c28f49867fd
+require github.com/pojntfx/tinynet v0.0.0-20201223174203-a55e21b95c6a
--- lib/capabilities/capabilities.ts
+++ lib/capabilities/capabilities.ts
@@ -1,9 +1,9 @@
 export default {
-  net_socket: ["berkeley_sockets_socket"],
-  net_send: ["berkeley_sockets_send"],
-  net_receive: ["berkeley_sockets_recv"],
-  net_bind: ["berkeley_sockets_bind"],
-  net_listen: ["berkeley_sockets_listen"],
-  net_accept: ["berkeley_sockets_accept"],
-  net_connect: ["berkeley_sockets_connect"],
+  net_socket: ["unisockets_socket"],
+  net_send: ["unisockets_send"],
+  net_receive: ["unisockets_recv"],
+  net_bind: ["unisockets_bind"],
+  net_listen: ["unisockets_listen"],
+  net_accept: ["unisockets_accept"],
+  net_connect: ["unisockets_connect"],
 };
