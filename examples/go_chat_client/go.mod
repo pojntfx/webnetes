4e5c53e86952d1319e37c8e499c3f8160293d0f9
Felix Pojtinger <felix@pojtinger.com>
2021-01-18 20:34:15 +0000

    feat: Add Go chat client example

---
+++ examples/go_chat_client/go.mod
@@ -0,0 +1,5 @@
+module github.com/alphahorizonio/webnetes/examples/go_chat_client
+
+go 1.15
+
+require github.com/alphahorizonio/tinynet v0.0.0-20210110022925-42393636bf7f
---
+++ examples/go_chat_client/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/alphahorizonio/tinynet/pkg/tinynet"
+)
+
+func main() {
+	// Parse flags
+	raddr := flag.String("raddr", "127.0.0.1:4206", "Address to connect to")
+	flag.Parse()
+
+	// Dial
+	conn, err := tinynet.Dial("tcp", *raddr)
+	if err != nil {
+		fmt.Println("could not dial", err)
+
+		os.Exit(1)
+	}
+
+	fmt.Println("Connected to", *raddr)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// Read from stdin & send
+	go func(innerWg *sync.WaitGroup) {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			out, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("could not read from stdin", err)
+
+				os.Exit(1)
+			}
+
+			if n, err := conn.Write([]byte(out)); err != nil {
+				if n == 0 {
+					break
+				}
+
+				fmt.Println("could not write from connection, removing connection", err)
+
+				break
+			}
+		}
+
+		fmt.Println("Disconnected")
+
+		if err := conn.Close(); err != nil {
+			fmt.Println("could not close connection", err)
+		}
+
+		innerWg.Done()
+	}(&wg)
+
+	// Receive & print
+	go func(innerWg *sync.WaitGroup) {
+		for {
+			buf := make([]byte, 1038)
+			if n, err := conn.Read(buf); err != nil {
+				if n == 0 {
+					break
+				}
+
+				fmt.Println("could not read from connection, removing connection", err)
+
+				break
+			}
+
+			fmt.Printf("%v", string(buf))
+		}
+
+		fmt.Println("Disconnected")
+
+		if err := conn.Close(); err != nil {
+			fmt.Println("could not close connection", err)
+		}
+
+		innerWg.Done()
+	}(&wg)
+
+	wg.Wait()
+}
